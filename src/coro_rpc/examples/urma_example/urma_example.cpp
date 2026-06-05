389e202555705833c0e037c979d26032b991454c
lixu <lixu@mooncake.dev>
2026-06-05 13:07:30 +0800

    update

--- src/coro_rpc/examples/urma_example/urma_example.cpp
+++ src/coro_rpc/examples/urma_example/urma_example.cpp
@@ -35,18 +35,25 @@ std::string_view echo(std::string_view data) { return data; }
 
 // The basic example about how to start a rpc connection over URMA.
 void basic_example() {
+  ELOG_INFO << "basic_example: starting";
   coro_rpc_client client;
   coro_rpc_server server;
 
+  ELOG_INFO << "basic_example: calling server.init_urma";
   server.init_urma();
+  ELOG_INFO << "set_option: registering handler";
   server.register_handler<echo>();
+  ELOG_INFO << "set_option: handler registered";
+  ELOG_INFO << "set_option: calling server.async_start";
   auto future = server.async_start();
+  ELOG_INFO << "set_option: server.async_start returned";
   if (future.hasResult()) {
     ELOG_ERROR << future.result().value().message();
     return;
   }
 
   // Client connects to server - URMA is auto-initialized from server config
+  ELOG_INFO << "set_option: server address=" << server.address() << " port=" << server.port();
   auto ec = syncAwait(client.connect(std::string{server.address()} + ":" +
                                      std::to_string(server.port())));
   if (ec) {
@@ -68,7 +75,9 @@ void basic_example() {
 
 // This example is about how to configure the detail URMA option.
 void set_option() {
+  ELOG_INFO << "set_option: starting";
   /* init global device, should call before any other call */
+  ELOG_INFO << "set_option: initializing global urma device";
   coro_io::get_global_urma_device(coro_io::urma_init_config_t{
       .dev_name = "bonding_dev_0",  /*URMA device name, default is empty, which means choice
                           the first URMA device*/
@@ -80,8 +89,10 @@ void set_option() {
           },
       .eid_index = 0 /*EID index to use*/});
 
+  ELOG_INFO << "set_option: creating client";
   coro_rpc_client client;
   coro_rpc_client::config conf;
+  ELOG_INFO << "set_option: client created, configuring urma";
   auto urma_config = coro_io::urma_socket_t::config_t{
       .recv_buffer_cnt = 4,  // buffer cnt of recv queue
       .send_buffer_cnt = 4,   // buffer cnt of send queue
@@ -89,21 +100,31 @@ void set_option() {
       .device_name = "bonding_dev_0",  // empty means auto-select
       .eid_index = 0      // EID index
   };
+  ELOG_INFO << "set_option: calling client.init_urma";
   if (!client.init_urma(urma_config)) {
     ELOG_ERROR << "URMA client init failed";
     return;
   }
+  ELOG_INFO << "set_option: client.init_urma succeeded";
 
+  ELOG_INFO << "set_option: creating server";
   coro_rpc_server server;
+  ELOG_INFO << "set_option: calling server.init_urma";
   server.init_urma(urma_config);
+  ELOG_INFO << "set_option: server.init_urma done";
 
+  ELOG_INFO << "set_option: registering handler";
   server.register_handler<echo>();
+  ELOG_INFO << "set_option: handler registered";
+  ELOG_INFO << "set_option: calling server.async_start";
   auto future = server.async_start();
+  ELOG_INFO << "set_option: server.async_start returned";
   if (future.hasResult()) {
     ELOG_ERROR << future.result().value().message();
     return;
   }
 
+  ELOG_INFO << "set_option: server address=" << server.address() << " port=" << server.port();
   auto ec = syncAwait(client.connect(std::string{server.address()} + ":" +
                                      std::to_string(server.port())));
   if (ec) {
@@ -124,7 +145,10 @@ void set_option() {
 
 int main() {
   easylog::logger<>::instance().set_min_severity(easylog::Severity::INFO);
+  ELOG_INFO << "URMA example main started";
   set_option();
+  ELOG_INFO << "set_option completed, now basic_example";
   basic_example();
+  ELOG_INFO << "basic_example completed";
   return 0;
 }
