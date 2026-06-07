715a743996f28f29bdc455b9128126bd153caec2
lixu <lixu@mooncake.dev>
2026-06-07 10:56:25 +0800

    update

---
+++ include/ylt/coro_io/urma/urma_io.hpp
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2025, Alibaba Group Holding Limited;
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <algorithm>
+#include <cstring>
+#include <vector>
+
+#include "ylt/coro_io/coro_io.hpp"
+#include "ylt/coro_io/urma/urma_socket.hpp"
+
+namespace coro_io {
+namespace detail {
+
+template <typename AsioBuffer, typename BufferSequence>
+void make_urma_buffers(std::vector<AsioBuffer>& result,
+                       BufferSequence& buffers) {
+  auto first = asio::buffer_sequence_begin(buffers);
+  auto last = asio::buffer_sequence_end(buffers);
+  for (; first != last; ++first) result.emplace_back(*first);
+}
+
+template <typename Buffer>
+async_simple::coro::Lazy<std::pair<std::error_code, std::size_t>>
+async_urma_read(urma_socket_t& socket, Buffer&& raw_buffer, bool read_some) {
+  if (!socket.get_executor().running_in_this_thread())
+    co_await dispatch(socket.get_executor());
+
+  std::vector<asio::mutable_buffer> buffers;
+  make_urma_buffers(buffers, raw_buffer);
+  std::size_t completed = 0;
+  for (auto& buffer : buffers) {
+    if (socket.remain_read_buffer_size()) {
+      auto count =
+          socket.consume(static_cast<char*>(buffer.data()), buffer.size());
+      buffer += count;
+      completed += count;
+    }
+    while (buffer.size()) {
+      auto [ec, length] =
+          co_await async_io<std::pair<std::error_code, std::size_t>>(
+              [&](auto&& callback) {
+                socket.post_recv(std::move(callback));
+              },
+              socket);
+      if (ec) co_return std::pair{ec, completed};
+      auto recv = socket.get_recv_buffer();
+      auto count = std::min<std::size_t>(length, buffer.size());
+      std::memcpy(buffer.data(), reinterpret_cast<void*>(recv.addr), count);
+      buffer += count;
+      completed += count;
+      socket.set_read_buffer_len(count, length - count);
+      if (read_some) co_return std::pair{std::error_code{}, completed};
+    }
+  }
+  co_return std::pair{std::error_code{}, completed};
+}
+
+}  // namespace detail
+
+template <typename Buffer>
+async_simple::coro::Lazy<std::pair<std::error_code, std::size_t>> async_write(
+    urma_socket_t& socket, Buffer&& raw_buffer) {
+  if (!socket.get_executor().running_in_this_thread())
+    co_await dispatch(socket.get_executor());
+
+  std::vector<asio::const_buffer> buffers;
+  detail::make_urma_buffers(buffers, raw_buffer);
+  std::size_t completed = 0;
+  while (!buffers.empty()) {
+    auto buffer = socket.get_send_buffer();
+    if (!buffer)
+      co_return std::pair{
+          std::make_error_code(std::errc::no_buffer_space), completed};
+    std::size_t length = 0;
+    while (!buffers.empty() && length < socket.get_buffer_size()) {
+      auto count = std::min<std::size_t>(
+          buffers.front().size(), socket.get_buffer_size() - length);
+      std::memcpy(static_cast<char*>(buffer.addr) + length,
+                  buffers.front().data(), count);
+      length += count;
+      buffers.front() += count;
+      if (buffers.front().size() == 0) buffers.erase(buffers.begin());
+    }
+    if (socket.sent_request_count() >= socket.get_config().send_buffer_cnt) {
+      auto ec = co_await socket.waiting_write_over();
+      if (ec) co_return std::pair{ec, completed};
+    }
+    auto result =
+        co_await async_io<std::pair<std::error_code, std::size_t>>(
+            [&](auto&& callback) {
+              socket.post_send(std::move(buffer), length,
+                               std::move(callback));
+            },
+            socket);
+    if (result.first) co_return std::pair{result.first, completed};
+    completed += length;
+  }
+  co_return std::pair{std::error_code{}, completed};
+}
+
+template <typename Buffer>
+async_simple::coro::Lazy<std::pair<std::error_code, std::size_t>> async_read(
+    urma_socket_t& socket, Buffer&& buffer) {
+  return detail::async_urma_read(socket, std::forward<Buffer>(buffer), false);
+}
+
+template <typename Buffer>
+async_simple::coro::Lazy<std::pair<std::error_code, std::size_t>>
+async_read_some(urma_socket_t& socket, Buffer&& buffer) {
+  return detail::async_urma_read(socket, std::forward<Buffer>(buffer), true);
+}
+
+}  // namespace coro_io
